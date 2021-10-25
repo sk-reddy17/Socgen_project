30d0e32d918fd38a54f0b88e95aad54b056ff9dd
sk-reddy17 <shashikumar.reddy80@gmail.com>
2021-10-25 09:44:54 +0530

    calulator spring boot

--- Hello/src/main/java/com/shashi/demo/controller/HelloController.java
+++ Hello/src/main/java/com/shashi/demo/controller/HelloController.java
@@ -1,22 +1,42 @@
 package com.shashi.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@GetMapping("/hello")
-	public String sayWish()
-	{
-		return "Hello SK Reddy";
-	}
-
-	@GetMapping("/wish")
-public String wish(@RequestParam String name)
+	@GetMapping("/add/{x}/{y}")
+public int add(@PathVariable int x, @PathVariable int y)
 {
-	return "Happy Birthday" + name;
+		return x + y;
 }
+	@GetMapping("/sub/{x}/{y}")
+	public int sub(@PathVariable int x, @PathVariable int y)
+	{
+			return x - y;
+	}
+	@GetMapping("/mul/{x}/{y}")
+	public double mul(@PathVariable double x, @PathVariable double y)
+	{
+			return x * y;
+	}
+	@GetMapping("/div/{x}/{y}")
+	public double div(@PathVariable double x, @PathVariable double y)
+	{
+			return x / y;
+	}
+	@GetMapping("/pow/{x}/{y}")
+	public double pow(@PathVariable double x, @PathVariable double y)
+	{
+			return Math.pow(x, y);
+	}@GetMapping("/root/{x}")
+	public double root(@PathVariable double x)
+	{
+			return Math.pow(x, 0.5);
+	}
+	
 
 }
