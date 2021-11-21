893326bb0e1191fad74c6467d70279aedfce2313
sk-reddy17 <67694361+sk-reddy17@users.noreply.github.com>
2021-11-22 00:07:28 +0530

    Delete CaseStudy directory

--- CaseStudy/APIGateway/src/main/java/com/shashi/APIGateway/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.shashi.APIGateway.service;
-
-import com.shashi.APIGateway.model.Mail;
-
-public interface MailService {
-	public void sendEmail(Mail mail);
-}
--- CaseStudy/CompanyService/src/main/java/com/shashi/CompanyService/dao/IpoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shashi.CompanyService.dao;
-
-import com.shashi.CompanyService.model.Ipo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IpoRepository extends MongoRepository<Ipo, String> {
-}
